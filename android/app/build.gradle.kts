61a91d9fddaacba03bfd08cea92a68d5c66985cc
Navin Kumar Verma <kumarnavinverma7@gmail.com>
2026-08-22 12:41:17 +0530

    chore: update compileSdk to dynamic flutter version and enable verbose logging for installer binary copying

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -14,7 +14,7 @@ if (keystorePropertiesFile.exists()) {
 
 android {
     namespace = "com.navnit.speedshare"
-    compileSdk = 37
+    compileSdk = flutter.compileSdkVersion
     ndkVersion = "27.0.12077973"
 
     compileOptions {
